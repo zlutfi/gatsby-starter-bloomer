1307ee340611784e261946798d441297f5f99028
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-15 11:16:06 -0400

    Updated pages with Bloomer components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,37 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Section, Container, Title, Subtitle, Button } from "bloomer"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Hero from "../components/hero"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Hero />
+    <body>
+      <Section>
+        <Container>
+          <Title>Hi people</Title>
+          <Subtitle>
+            Welcome to your new Gatsby site. Now go build something{" "}
+            <strong>great</strong>.
+          </Subtitle>
+          <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+            <Image />
+          </div>
+          <Link to="/page-2/">
+            <Button isColor="primary" isOutlined>
+              Go to page 2
+            </Button>
+          </Link>
+
+          {/* <Link to="/page-2/">Go to page 2</Link> */}
+        </Container>
+      </Section>
+    </body>
   </Layout>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Section, Container, Title, Subtitle, Button } from "bloomer"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,9 +8,22 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <body>
+      <Section>
+        <Container>
+          <Title>Hi from the second page</Title>
+          <Subtitle>
+            Welcome to page <strong>2</strong>
+          </Subtitle>
+          <Link to="/">
+            <Button isColor="primary" isOutlined>
+              Go back to the homepage
+            </Button>
+          </Link>
+          {/* <Link to="/">Go back to the homepage</Link> */}
+        </Container>
+      </Section>
+    </body>
   </Layout>
 )
 
