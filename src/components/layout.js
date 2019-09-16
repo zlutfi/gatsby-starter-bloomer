ff2c453039651711bf5bc02130ceb7de302cc16a
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-16 11:55:10 -0400

    Updated layout for sticky footer support

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,9 +9,8 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import MainFooter from "./footer"
-// import "./layout.css"
+import Navbar from "./navbar"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,20 +25,11 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-
-      {/* <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        > */}
-      <main>{children}</main>
-      <MainFooter />
-
-      {/* </div> */}
+      <div className="site">
+        <Navbar siteTitle={data.site.siteMetadata.title} />
+        <main className="site-content">{children}</main>
+        <MainFooter />
+      </div>
     </>
   )
 }
