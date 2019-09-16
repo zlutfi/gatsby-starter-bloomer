26c4cd68fdfc62e9f07066bcce04c0d32950b7fe
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-16 11:56:19 -0400

    Updated layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import React from "react"
 
+import { Title, Section, Container } from "bloomer"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Section>
+      <Container>
+        <Title>NOT FOUND</Title>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Container>
+    </Section>
   </Layout>
 )
 
