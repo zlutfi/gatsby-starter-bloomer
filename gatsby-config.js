18e92556cab18e119efd9c36a284af583045dbd7
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-16 13:42:07 -0400

    Updated description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Gatsby + Bloomer`,
-    description: `Kick off your next, great Gatsby project with this default starter with Bloomer components. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Kick off your next, great Gatsby project with this default starter with Bloomer components. `,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -30,6 +30,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
