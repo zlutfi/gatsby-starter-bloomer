ba6e5e6e0fe51c5fcb5a2bdc532b4ce3aa2dd4e9
zlutfi <33373366+zlutfi@users.noreply.github.com>
2019-09-30 10:20:14 -0400

    Updated layout

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,6 +1,15 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Section, Container, Title, Subtitle, Button } from "bloomer"
+import {
+  Section,
+  Container,
+  Title,
+  Subtitle,
+  Button,
+  Icon,
+  Column,
+  Columns,
+} from "bloomer"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -10,13 +19,20 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <Section className="has-background-white">
       <Container>
-        <Title isSpaced>Hi from the second page</Title>
-        <Subtitle>
-          Welcome to page <strong>2</strong>
-        </Subtitle>
-        <Link to="/">
-          <Button isColor="primary">Go back to the homepage</Button>
-        </Link>
+        <Columns>
+          <Column>
+            {/* Page title */}
+            <Title isSpaced>Page 2</Title>
+            <Subtitle>This is the second page.</Subtitle>
+            {/* Back to homepage button */}
+            <Link to="/">
+              <Button isColor="primary" className="is-rounded" id="is-spaced">
+                <Icon className="fa fa-arrow-left fa-sm" />
+                <span>Go back to the homepage</span>
+              </Button>
+            </Link>
+          </Column>
+        </Columns>
       </Container>
     </Section>
   </Layout>
